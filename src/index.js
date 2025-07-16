ffe2d27eb36c210f51b825bd7916580d80bae49d
mikhail2013ru <mikhail2013ru@gmail.com>
2025-07-16 20:02:04 +0300

    обновление адаптива

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles/main.scss';
-import { Tooltip, Toast, Popover } from 'bootstrap';
+// import { Tooltip, Toast, Popover } from 'bootstrap';
 // Импорт Bootstrap JS
 // import 'bootstrap';
