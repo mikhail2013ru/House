9bfb310aaf06179ed48f2d4b7ac466bef6ae720b
mikhail2013ru <mikhail2013ru@gmail.com>
2025-08-01 17:57:38 +0300

    мобильное меню

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import './styles/main.scss';
 document.addEventListener('DOMContentLoaded', function() {
     console.log('true');
     
-    // const burger = document.querySelector('.header__burger');
+    const burger = document.querySelector('.header__burger');
     const closeBtn = document.querySelector('.header__mobile-close');
     const mobileNav = document.querySelector('.header__mobile-menu-nav');
     const overlay = document.querySelector('.header__overlay');
 
-    // burger.addEventListener('click', function() {
-    //     mobileNav.classList.add('active');
-    //     overlay.classList.add('active');
-    //     document.body.style.overflow = 'hidden'; // Блокируем скролл страницы
-    // });
+    burger.addEventListener('click', function() {
+        // mobileNav.classList.add('active');
+        // mobileNav.style.display = 'block';
+        mobileNav.classList.add('active');
+        overlay.classList.add('active');
+        document.body.style.overflow = 'hidden'; // Блокируем скролл страницы
+    });
 
     closeBtn.addEventListener('click', function() {
+        // mobileNav.style.display = 'none';
         mobileNav.classList.remove('active');
         overlay.classList.remove('active');
         document.body.style.overflow = ''; // Восстанавливаем скролл
